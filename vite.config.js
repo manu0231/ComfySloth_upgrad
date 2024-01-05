f910abf81319b7b709e451d05b938815cb38fdff
= <mauryasourav998@gmail.com>
2024-01-06 00:47:26 +0530

    toml

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   server: {
     proxy: {
       // '/api': 'http://localhost:3000',
-      '/api': 'https://storeserver-production-bc7e.up.railway.app',
+      // '/api': 'https://storeserver-production-bc7e.up.railway.app',
     },
   },
   plugins: [react()],
